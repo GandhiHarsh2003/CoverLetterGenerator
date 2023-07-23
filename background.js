f8ac59d8eb7cd50caa3bd974d1f29d254a8d7d29
Ajifolahan <momoreajifolahan@gmail.com>
2023-07-23 01:25:07 -0400

    Added resume input and parsing resume feature

--- background.js
+++ background.js
@@ -1,119 +1,15 @@
-//Working on popping up the webpage on particular websites. Don't delete. 
-// chrome.runtime.onInstalled.addListener(function() {
-//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//       chrome.declarativeContent.onPageChanged.addRules([{
-//         conditions: [
-//           new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: {urlContains: 'careers'}
-//           })
-//         ],
-//         actions: [new chrome.declarativeContent.ShowPageAction()]
-//       }]);
-//     });
-//   });
-  
-
-  // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  //   if (changeInfo.url && changeInfo.url.includes("careers")) {
-  //     chrome.action.enable(tabId);
-  //   } else {
-  //     chrome.action.disable(tabId);
-  //   }
-  // });
-  
-  //To get the url of the page
-  function getActiveTabUrl() {
+function getActiveTabUrl() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       if (tabs && tabs.length > 0) {
-        var activeTab = tabs[0];
-        var url = activeTab.url;
+        let activeTab = tabs[0];
+        let url = activeTab.url;
         console.log(url);
       }
     });
-  }
+}
   
-  chrome.tabs.onActivated.addListener(function(activeInfo) {
+chrome.tabs.onActivated.addListener(function(activeInfo) {
     getActiveTabUrl();
-  });
+});
   
   getActiveTabUrl();
-  
- // To get the text on the page
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status === 'complete') {
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tabId },
-          function: getJobText,
-        },
-        function(result) {
-          if (result && result.length > 0) {
-            var pageText = result[0].result;
-            console.log("Job description: "+ pageText);
-          }
-        }
-      );
-    }
-  });
-  
-  // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  //   if (changeInfo.status === 'complete') {
-  //     chrome.scripting.executeScript(
-  //       {
-  //         target: { tabId: tabId },
-  //         function: function() {
-  //           var jobText = getJobText();
-  //           var skillsText = getSkillsText();
-  //           var aboutText = getAboutCompanyText();
-  //           return { jobText, skillsText, aboutText };
-  //         },
-  //       },
-  //       function(result) {
-  //         if (result && result.length > 0) {
-  //           var { jobText, skillsText, aboutText } = result[0].result;
-  //           console.log("Job description: " + jobText);
-  //           console.log("Skills: " + skillsText);
-  //           console.log("About the company: " + aboutText);
-     
-  //       }
-  //   });
-  //   }
-  // });
-  
-
-  // returns job description
-  function getJobText() {
-    let descriptionElement = document.querySelector("[class*=job-description]");
-    if (descriptionElement) {
-      return descriptionElement.innerText;
-    } else {
-      return "Nothing's retrieved";
-    }
-  }
-
- //returns the skills listed on job site
-  function getSkillsText(){
-    let skillsElement = document.querySelector("[class*=skills]");
-    if (skillsElement){
-      return skillsElement.innerText;
-    } else {
-      return "No skills retrieved";
-    }
-  }
-
-  //returns company info listed
-  function getAboutCompanyText(){
-    let aboutElement = document.querySelector("[class*=about]");
-    if (aboutElement){
-      return aboutElement.innerText;
-    } else {
-      return "No about the company retreived";
-    }
-  }
-
-
-  // function getPageText() {
-  //    return document.body.innerText;
-  //  }
-  
-  
--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+import apiKey from './keys.js';
+
 document.addEventListener("DOMContentLoaded", function() {
-    var closeButton = document.getElementById("close");
-  
-    closeButton.addEventListener("click", function() {
-      chrome.extension.getViews({ type: "popup" })[0].close();
-    });
+  const closeButton = document.getElementById("close");
+  closeButton.addEventListener("click", function() {
+    chrome.extension.getViews({ type: "popup" })[0].close();
   });
+
+  const url = 'https://docwire-doctotext.p.rapidapi.com/extract_text';
+  const doc = document.getElementById('resumeFile');
+  doc.onchange = async () => {
+    const selectedFile = doc.files[0];
+    console.log("Selected file name:", selectedFile.name);
+    const data = new FormData();
+    data.append('file', selectedFile);
+    data.append('filename', selectedFile.name);
+    data.append('page', '1');
+    const options = {
+      method: 'POST',
+      headers: {
+        'X-RapidAPI-Key': apiKey,
+        'X-RapidAPI-Host': 'docwire-doctotext.p.rapidapi.com'
+      },
+      body: data
+    };
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+    //generate cover letter on click. 
+});
