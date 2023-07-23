83b88e5177d1130406770878c6f3e367d4711b80
kevin-rodri <kr399880@gmail.com>
2023-07-23 18:25:56 -0400

    merge conflicts fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import apiKey from './keys.js';
-
+import APIKEY from './apiKey.js';
 document.addEventListener("DOMContentLoaded", function() {
   const closeButton = document.getElementById("close");
   closeButton.addEventListener("click", function() {
@@ -31,5 +31,44 @@ document.addEventListener("DOMContentLoaded", function() {
       console.error(error);
     }
   }
-    //generate cover letter on click. 
+
+  const optionss = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': APIKEY,
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    }
+  };
+  
+
+  
+  const submitButton = document.getElementById('submit');
+  submitButton.addEventListener("click", function() {
+    let value = document.getElementById('box').value;
+    console.log("value: " + value);
+
+    const query = encodeURIComponent(value);
+    console.log("Encoded query:", query);
+    const urlQuery = `https://jsearch.p.rapidapi.com/search?query=${query}`;
+    fetch(urlQuery, optionss)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        const jobDescription = response.data[0].job_description;
+        const qualifications = response.data[0].job_highlights.Qualifications;
+        const responsibilities = response.data[0].job_highlights.Responsibilities;
+        console.log(jobDescription);
+        console.log(qualifications);
+        console.log(responsibilities);
+      })
+      .catch(err => console.error(err));
+  });
+
+
+     //generate cover letter on click. 
 });
+   
+
+  
+  
+  
