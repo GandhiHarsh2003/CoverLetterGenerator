871097bf4b53ee2bda15e3afa00ff03b15d7338b
Ajifolahan <momoreajifolahan@gmail.com>
2023-07-23 18:38:15 -0400

    Removed unused comments

--- background.js
+++ background.js
@@ -14,32 +14,4 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
   
 
   getActiveTabUrl();
-  
- // To get the text on the page
-//  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//   if (changeInfo.status === 'complete') {
-//     chrome.scripting.executeScript(
-//       {
-//         target: { tabId: tabId },
-//         function: getJobInformation,
-//       },
-//       function(result) {
-//         if (result && result.length > 0) {
-//           var pageText = result[0].result;
-//           console.log("Job description: " + pageText);
-          
-//         }
-//       }
-//     );
-//   }
-// });
-
-  
-
-
-  // function getPageText() {
-  //    return document.body.innerText;
-  //  }
-  
-  
 
