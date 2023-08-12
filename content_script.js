155281369046d71f0f991dd3bdcf39574bc15372
Ajifolahan <momoreajifolahan@gmail.com>
2023-08-12 17:54:48 -0400

    Still working on the prompt

--- content_script.js
+++
@@ -1,7 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-
-    if (changeInfo.url && changeInfo.url.includes("careers")) {
-    
-      chrome.runtime.sendMessage({type: "openPopup"});
-    }
-});
